22788df89b0a5dd4496af343cc8e13eb683bf08c
kradova <ksenia.radova@gmail.com>
2022-11-14 20:53:41 +0300

    + using findElement()

--- src/main/java/com/kradova/Main.java
+++ src/main/java/com/kradova/Main.java
@@ -1,6 +1,8 @@
 package com.kradova;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
@@ -8,15 +10,9 @@ public class Main {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         driver.get("http://practice.bpbonline.com/");
-        System.out.println(driver.getTitle());
-        if (driver.getPageSource().contains("Welcome to BPB PUBLICATIONS")) {
-            System.out.println("Page is loaded");
-        }
         driver.manage().window().maximize();
-        driver.navigate().to("http://www.selenium.dev");
-        if (driver.getCurrentUrl().contains("selenium")) {
-            System.out.println("Page is now changed to Selenium website");
-        }
+        WebElement myAccountlink = driver.findElement(By.linkText("My Account"));
+        myAccountlink.click();
         driver.close();
     }
 }
