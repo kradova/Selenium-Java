6eb72a0a266f0ddfaa47b8f2a0a0e9cb8e03e0e8
kradova <ksenia.radova@gmail.com>
2022-11-14 21:08:44 +0300

    + using findElements(By.xpath())

--- src/main/java/com/kradova/Main.java
+++ src/main/java/com/kradova/Main.java
@@ -5,14 +5,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         driver.get("http://practice.bpbonline.com/");
         driver.manage().window().maximize();
-        WebElement myAccountlink = driver.findElement(By.linkText("My Account"));
-        myAccountlink.click();
+        List<WebElement> allLinks = driver.findElements(By.xpath("//a"));
+        for (WebElement lnk : allLinks) {
+            System.out.println(lnk.getText());
+        }
         driver.close();
     }
 }
