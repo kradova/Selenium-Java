df77a4c8301500d4bd5a74639dfa4d62c8429eae
kradova <ksenia.radova@gmail.com>
2022-11-14 20:05:46 +0300

    + ChromeDriver to the app

--- src/main/java/com/kradova/Main.java
+++ src/main/java/com/kradova/Main.java
@@ -3,10 +3,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
-        driver.get("https://selenium.dev");
-        driver.quit();
+        driver.get("http://practice.bpbonline.com/");
+        Thread.sleep(5000);
+        driver.close();
     }
 }
