a61485482be34c741882fd9f22cbcd57932bf6bd
kradova <ksenia.radova@gmail.com>
2022-11-14 20:47:09 +0300

    + using getTitle(), getPageSource() and getCurrentUrl() methods

--- src/main/java/com/kradova/Main.java
+++ src/main/java/com/kradova/Main.java
@@ -1,4 +1,5 @@
 package com.kradova;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,8 +8,15 @@ public class Main {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         driver.get("http://practice.bpbonline.com/");
+        System.out.println(driver.getTitle());
+        if (driver.getPageSource().contains("Welcome to BPB PUBLICATIONS")) {
+            System.out.println("Page is loaded");
+        }
         driver.manage().window().maximize();
         driver.navigate().to("http://www.selenium.dev");
+        if (driver.getCurrentUrl().contains("selenium")) {
+            System.out.println("Page is now changed to Selenium website");
+        }
         driver.close();
     }
 }
