121152d5e6ce501fb1c6d7ca89c311eefad5e6b0
kradova <ksenia.radova@gmail.com>
2022-11-16 14:41:17 +0300

    + implicitlyWait()

--- src/main/java/com/kradova/Main.java
+++ src/main/java/com/kradova/Main.java
@@ -6,20 +6,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         //driver.get("http://practice.bpbonline.com/");
-        driver.get("https://the-internet.herokuapp.com/checkboxes");
-        driver.manage().window().maximize();
-        WebElement firstCheckbox = driver.findElement(By.cssSelector("#checkboxes"));
-        System.out.println(firstCheckbox.isDisplayed());
-        System.out.println(firstCheckbox.isEnabled());
-        System.out.println(firstCheckbox.isSelected());
-        firstCheckbox.click();
-        System.out.println(firstCheckbox.isSelected());
+        //driver.get("https://the-internet.herokuapp.com/checkboxes");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://www.practice.bpbonline.com/");
+        driver.findElement(By.linkText("My Account")).click();
+        driver.findElement(By.name("email_address")).sendKeys("bpb@bpb.com");
+        driver.findElement(By.name("password")).sendKeys("bpb@123");
+        driver.findElement(By.id("tdb1")).click();
+
+        driver.findElement(By.linkText("Log Off")).click();
+        driver.findElement(By.linkText("Continue")).click();
+
         driver.close();
     }
 }
