f849770e41d8b56cf9e41d0580446761a6eddbb0
kradova <ksenia.radova@gmail.com>
2022-11-16 15:02:17 +0300

    + WebDriverWait() and Duration class

--- src/main/java/com/kradova/Main.java
+++ src/main/java/com/kradova/Main.java
@@ -4,7 +4,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -20,6 +24,11 @@ public class Main {
         driver.findElement(By.name("email_address")).sendKeys("bpb@bpb.com");
         driver.findElement(By.name("password")).sendKeys("bpb@123");
         driver.findElement(By.id("tdb1")).click();
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+
+        new WebDriverWait(driver,  Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(By.linkText("Log Off")));
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.findElement(By.linkText("Log Off")).click();
         driver.findElement(By.linkText("Continue")).click();
