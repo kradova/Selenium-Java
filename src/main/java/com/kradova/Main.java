661ea63dcb9be39b84b4c359f38cc42a937ea0ac
kradova <ksenia.radova@gmail.com>
2022-11-19 13:52:03 +0300

    + update user profile

--- src/main/java/com/kradova/Main.java
+++ src/main/java/com/kradova/Main.java
@@ -24,16 +24,28 @@ public class Main {
         driver.findElement(By.name("email_address")).sendKeys("bpb@bpb.com");
         driver.findElement(By.name("password")).sendKeys("bpb@123");
         driver.findElement(By.id("tdb1")).click();
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
 
-        new WebDriverWait(driver,  Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(By.linkText("Log Off")));
+        driver.findElement(By.linkText("View or change my account information.")).click();
+        //new WebDriverWait(driver,  Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(By.linkText("Log Off")));
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        if (driver.findElement(By.xpath("//input[@value = 'm']")).isSelected()) {
+            driver.findElement(By.xpath("//input[@value = 'f']")).click();
+        } else {
+            driver.findElement(By.xpath("//input[@value = 'm']")).click();
+        }
+        driver.findElement(By.name("telephone")).clear();
+        driver.findElement(By.name("telephone")).sendKeys("23838393");
+        driver.findElement(By.id("tdb5")).click();
+        if (driver.getPageSource().contains("account has been successfully updated")) {
+            System.out.println("Change profile successful");
+        } else {
+            System.out.println("Profile information not changed");
+        }
 
         driver.findElement(By.linkText("Log Off")).click();
         driver.findElement(By.linkText("Continue")).click();
-
         driver.close();
     }
 }
+
 
